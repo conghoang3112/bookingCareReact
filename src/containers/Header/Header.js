c2af8d871f6dd936a34879169c881dec426e3d83
Cong Hoang <conghoang3112xxx@gmail.com>
2024-02-13 23:36:10 +0700

    Assign rights to Admin in Menu

--- src/containers/Header/Header.js
+++ src/containers/Header/Header.js
@@ -6,6 +6,7 @@ import Navigator from '../../components/Navigator';
 import { adminMenu } from './menuApp';
 import './Header.scss';
 import { LANGUAGES } from '../../utils/constant';
+import { FormattedMessage } from 'react-intl';
 
 class Header extends Component {
 
@@ -14,7 +15,7 @@ class Header extends Component {
     }
 
     render() {
-        const { processLogout, language } = this.props;
+        const { processLogout, language, userInfo } = this.props;
 
         return (
             <div className="header-container">
@@ -24,6 +25,10 @@ class Header extends Component {
                 </div>
 
                 <div className='languages'>
+                    <span className='welcome'>
+                        <FormattedMessage id="home-header.welcome" />
+                        {userInfo && userInfo.firstName ? userInfo.firstName : ''}!
+                    </span>
                     <span className={language === LANGUAGES.VI ? 'language-vi active' : 'language-vi '}
                         onClick={() => this.handleChangeLanguage(LANGUAGES.VI)}>VN</span>
                     <span className={language === LANGUAGES.EN ? 'language-en active' : 'language-en '}
@@ -44,7 +49,8 @@ class Header extends Component {
 const mapStateToProps = state => {
     return {
         isLoggedIn: state.user.isLoggedIn,
-        language: state.app.language
+        language: state.app.language,
+        userInfo: state.user.userInfo
     };
 };
 
