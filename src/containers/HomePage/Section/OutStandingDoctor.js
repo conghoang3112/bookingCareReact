22d1479f597205d65789e038337413de10158449
Cong Hoang <conghoang3112xxx@gmail.com>
2024-02-22 19:11:58 +0700

    update to load infomation featured doctor last week

--- src/containers/HomePage/Section/OutStandingDoctor.js
+++ src/containers/HomePage/Section/OutStandingDoctor.js
@@ -3,11 +3,31 @@ import { connect } from 'react-redux';
 import { FormattedMessage } from 'react-intl';
 import Slider from 'react-slick';
 import '../HomePage.scss';
+import * as actions from "../../../store/actions";
+import { LANGUAGES } from '../../../utils';
 
 class OutStandingDoctor extends Component {
-
+    constructor(props) {
+        super(props)
+        this.state = {
+            arrDoctor: []
+        }
+    }
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        if (prevProps.topDoctorsRedux !== this.props.topDoctorsRedux) {
+            this.setState({
+                arrDoctor: this.props.topDoctorsRedux
+            })
+        }
+    }
+    componentDidMount() {
+        this.props.loadTopDoctors();
+    }
     render() {
-
+        let { language } = this.props;
+        console.log('check topDoctorsRedux: ', this.props.topDoctorsRedux)
+        let arrDoctor = this.state.arrDoctor;
+        arrDoctor = arrDoctor.concat(arrDoctor).concat(arrDoctor);
         return (
             <div className='section-share section-outstanding-doctor'>
                 <div className='section-container'>
@@ -18,72 +38,35 @@ class OutStandingDoctor extends Component {
 
                     <div className='section-body'>
                         <Slider {...this.props.settings}>
-                            <div className='section-customize'>
-                                <div className='customize-boder'>
-                                    <div className='outer-bg'>
-                                        <div className='bg-image section-outstanding-doctor' />
-                                    </div>
-                                    <div className='position text-center'>
-                                        <div>Giáo sư tiến sĩ</div>
-                                        <div>Cơ xương khớp</div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className='section-customize'>
-                                <div className='customize-boder'>
-                                    <div className='outer-bg'>
-                                        <div className='bg-image section-outstanding-doctor' />
-                                    </div>
-                                    <div className='position text-center'>
-                                        <div>Giáo sư tiến sĩ</div>
-                                        <div>Cơ xương khớp</div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className='section-customize'>
-                                <div className='customize-boder'>
-                                    <div className='outer-bg'>
-                                        <div className='bg-image section-outstanding-doctor' />
-                                    </div>
-                                    <div className='position text-center'>
-                                        <div>Giáo sư tiến sĩ</div>
-                                        <div>Cơ xương khớp</div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className='section-customize'>
-                                <div className='customize-boder'>
-                                    <div className='outer-bg'>
-                                        <div className='bg-image section-outstanding-doctor' />
-                                    </div>
-                                    <div className='position text-center'>
-                                        <div>Giáo sư tiến sĩ</div>
-                                        <div>Cơ xương khớp</div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className='section-customize'>
-                                <div className='customize-boder'>
-                                    <div className='outer-bg'>
-                                        <div className='bg-image section-outstanding-doctor' />
-                                    </div>
-                                    <div className='position text-center'>
-                                        <div>Giáo sư tiến sĩ</div>
-                                        <div>Cơ xương khớp</div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className='section-customize'>
-                                <div className='customize-boder'>
-                                    <div className='outer-bg'>
-                                        <div className='bg-image section-outstanding-doctor' />
-                                    </div>
-                                    <div className='position text-center'>
-                                        <div>Giáo sư tiến sĩ</div>
-                                        <div>Cơ xương khớp</div>
-                                    </div>
-                                </div>
-                            </div>
+                            {arrDoctor && arrDoctor.length > 0
+                                && arrDoctor.map((item, index) => {
+                                    let imageBase64 = '';
+                                    if (item.image) {
+                                        imageBase64 = new Buffer(item.image, 'base64').toString('binary');
+                                        // `data:image/png;base64,${imageBuffer.toString('base64')}`
+                                    }
+                                    if (index === 0) {
+                                        console.log('check item: ', item);
+                                    }
+                                    let nameVi = `${item.positionData.valueVi}, ${item.lastName} ${item.firstName} `
+                                    let nameEn = `${item.positionData.valueEn}, ${item.lastName} ${item.firstName}`
+                                    return (
+                                        <div className='section-customize' key={index}>
+                                            <div className='customize-boder'>
+                                                <div className='outer-bg'>
+                                                    <div className='bg-image section-outstanding-doctor'
+                                                        style={{ backgroundImage: `url(${imageBase64})` }}
+                                                    />
+                                                </div>
+                                                <div className='position text-center'>
+                                                    <div>{language === LANGUAGES.VI ? nameVi : nameEn}</div>
+                                                    <div>Cơ xương khớp</div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    )
+                                })
+                            }
 
 
                         </Slider>
@@ -98,12 +81,15 @@ class OutStandingDoctor extends Component {
 
 const mapStateToProps = state => {
     return {
-        isLoggedIn: state.user.isLoggedIn
+        language: state.app.language,
+        isLoggedIn: state.user.isLoggedIn,
+        topDoctorsRedux: state.admin.topDoctors
     };
 };
 
 const mapDispatchToProps = dispatch => {
     return {
+        loadTopDoctors: () => dispatch(actions.fetchTopDoctor()),
     };
 };
 
--- src/services/userService.js
+++ src/services/userService.js
@@ -26,7 +26,11 @@ const getAllCodeService = (inputType) => {
     return axios.get(`/api/allcode?type=${inputType}`);
 }
 
+const getTopDoctorHomeService = (limit) => {
+    return axios.get(`api/top-doctor-home?limit=${limit}`)
+}
+
 export {
     handleLoginApi, getAllUser, createNewUserService, deleteUserService, editUserService,
-    getAllCodeService
+    getAllCodeService, getTopDoctorHomeService
 }
